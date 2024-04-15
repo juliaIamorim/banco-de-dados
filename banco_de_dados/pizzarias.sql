21391e3f6daea10667eb638cf6d9aaca140189ed
Júlia Isadora Amorim <110129192+juliaIamorim@users.noreply.github.com>
2024-04-15 14:17:24 +0000

    inicio

---
+++ banco_de_dados/pizzarias.sql
@@ -0,0 +1,9 @@
+-- BANCO DE DADOS DA PIZZARIA
+
+-- CREATE DOMAIN d_nomeProprio VARCHAR(40);
+-- CREATE DOMAIN d_telefone VARCHAR(15);
+-- CREATE DOMAIN d_moedas DECIMAL(15,2);
+
+CREATE TABLE ingredientes(
+idIngrediente SERIAL
+);
